9f773c71fdb4451fe3eeb267580b289ec0876462
cjoh <cjoh@clayjohnson.org>
2013-01-09 14:27:27 -0500

    Update application/views/mailer/application_received_text.php

--- application/views/mailer/application_received_text.php
+++ application/views/mailer/application_received_text.php
@@ -1,4 +1,8 @@
-Thanks for applying for <?= __('r.app_name') ?>!
+Thanks for applying for <?= __('r.app_name') ?>! We have received your application, and it is now being reviewed. 
+
+In terms of what to expect -- because of the volume of applications we're receiving, it will likely be a few weeks before you hear anything back from us. If your applicaiton seems promising, individual agencies will reach out to you to conduct interviews. If not, you will receive an email letting you know you have not been selected, or recommending that you apply to another project.
+
+Your application means a lot. Thank you for being willing to serve and to use your skills to better your government and your country. 
 
 For your records, here's your application:
 
