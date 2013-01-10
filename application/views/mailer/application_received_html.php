4c0f3adfe46a62a15cfb99f1d8b7b39095ca90b5
adamjacobbecker <ad@mbecker.cc>
2013-01-10 15:41:08 -0500

    fix typo

--- application/views/mailer/application_received_html.php
+++ application/views/mailer/application_received_html.php
@@ -1,11 +1,11 @@
 <p>Thanks for applying for <?= __('r.app_name') ?>! We have received your application, and it is now being reviewed.</p>
 
-<p>In terms of what to expect -- because of the volume of applications we're receiving, it will likely be a 
-few weeks before you hear anything back from us. If your applicaiton seems promising, individual 
-agencies will reach out to you to conduct interviews. If not, you will receive an email letting you 
+<p>In terms of what to expect -- because of the volume of applications we're receiving, it will likely be a
+few weeks before you hear anything back from us. If your application seems promising, individual
+agencies will reach out to you to conduct interviews. If not, you will receive an email letting you
 know you have not been selected, or recommending that you apply to another project.</p>
 
-<p>Your application means a lot. Thank you for being willing to serve and to use your skills to better your 
+<p>Your application means a lot. Thank you for being willing to serve and to use your skills to better your
 government and your country.</p>
 
 <p>For your records, here's your application:</p>
--- application/views/mailer/application_received_text.php
+++ application/views/mailer/application_received_text.php
@@ -1,8 +1,8 @@
-Thanks for applying for <?= __('r.app_name') ?>! We have received your application, and it is now being reviewed. 
+Thanks for applying for <?= __('r.app_name') ?>! We have received your application, and it is now being reviewed.
 
-In terms of what to expect -- because of the volume of applications we're receiving, it will likely be a few weeks before you hear anything back from us. If your applicaiton seems promising, individual agencies will reach out to you to conduct interviews. If not, you will receive an email letting you know you have not been selected, or recommending that you apply to another project.
+In terms of what to expect -- because of the volume of applications we're receiving, it will likely be a few weeks before you hear anything back from us. If your application seems promising, individual agencies will reach out to you to conduct interviews. If not, you will receive an email letting you know you have not been selected, or recommending that you apply to another project.
 
-Your application means a lot. Thank you for being willing to serve and to use your skills to better your government and your country. 
+Your application means a lot. Thank you for being willing to serve and to use your skills to better your government and your country.
 
 For your records, here's your application:
 
