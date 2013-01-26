46ba340e3c8f5c46eebcc45ec55e37c0cd5aaa7c
adamjacobbecker <ad@mbecker.cc>
2013-01-26 12:53:55 -0500

    prettier output from increment_deploy_timestamp task

--- application/tasks/increment_deploy_timestamp.php
+++ application/tasks/increment_deploy_timestamp.php
@@ -3,7 +3,8 @@
 class Increment_Deploy_Timestamp_Task extends Task {
 
 	public function run() {
-    print file_put_contents("deploy_timestamp.txt", time());
+    file_put_contents("deploy_timestamp.txt", time());
+    print "Incremented deploy timestamp.";
   }
 
 }
