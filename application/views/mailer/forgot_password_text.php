f0d200ea34be0d270872f8cc4f5f2f177cf8aad8
Jed Wood <jed@limechile.com>
2013-02-04 10:02:55 -0600

    in sign-up and password reset emails, change EasyBid to Hire-EZ

--- application/views/mailer/forgot_password_text.php
+++ application/views/mailer/forgot_password_text.php
@@ -1,3 +1,3 @@
-Here's a link to reset your EasyBid password: <?= route('reset_password', array($user->reset_password_token)) ?>
+Here's a link to reset your Hire-EZ password: <?= route('reset_password', array($user->reset_password_token)) ?>
 
 <?= __('r.email_signature_text') ?>
